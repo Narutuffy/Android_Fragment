86781fe90c1aa8499467a38d3cba8e82b58a2ae6
Vamsi <s.vamsirao@gmail.com>
2017-05-12 09:00:19 +0530

    Create fragments for body and legs too

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -36,12 +36,28 @@ public class AndroidMeActivity extends AppCompatActivity {
         headFragment.setImageIds(AndroidImageAssets.getHeads());
         headFragment.setListIndex(7);
 
+        BodyPartFragment bodyFragment= new BodyPartFragment();
+
+        bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+        bodyFragment.setListIndex(7);
+
+        BodyPartFragment legFragment= new BodyPartFragment();
+        legFragment.setImageIds(AndroidImageAssets.getLegs());
+        legFragment.setListIndex(7);
 
         FragmentManager fragmentManager=getSupportFragmentManager();
 
         fragmentManager.beginTransaction()
                 .add(R.id.head_container,headFragment)
                 .commit();
+
+        fragmentManager.beginTransaction().
+                add(R.id.body_container,bodyFragment).
+                commit();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.leg_container,legFragment).
+                commit();
 
     }
 }
